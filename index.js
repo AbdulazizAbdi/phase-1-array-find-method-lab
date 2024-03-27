9071cc702761fb4006370be221d07fe1d37a76b7
Abdulaziz Abdi <amigf76@gmail.com>
2024-03-27 17:39:00 +0300

    find methid lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record){
+    for(let element of record){
+        if(element.result === "W"){
+            return element.year;
+        }
+    }
+}
+
+record.find(superbowlWin);
